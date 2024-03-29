9673947ce4083581a62a8e0193276277231b7e93
newways <reactwebdew@gmail.com>
2024-03-29 16:15:15 +0100

    convex mutation - create board

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -2,8 +2,28 @@
 
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { useOrganization } from '@clerk/nextjs'
+import { useApiMutation } from '@/hooks/use-api-mutation'
+import { toast } from 'sonner'
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization()
+  const { mutate, pending } = useApiMutation(api.board.create)
+
+  const onClick = () => {
+    if (!organization) return
+
+    mutate({
+      orgId: organization.id,
+      title: 'Untitled',
+    })
+      .then((id) => {
+        toast.success('Board created')
+      })
+      .catch(() => toast.error('Failed to create board'))
+  }
   return (
     <div className='h-full flex flex-col items-center justify-center'>
       <Image src='/note.svg' height={110} width={110} alt='Empty' />
@@ -12,7 +32,9 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className='mt-6'>
-        <Button size='lg'>Create board</Button>
+        <Button disabled={pending} onClick={onClick} size='lg'>
+          Create board
+        </Button>
       </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { ConvexClientProvider } from '@/providers/convex-client-provider'
+import { Toaster } from '@/components/ui/sonner'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          <Toaster />
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   )
